c8a80d0e2d9862f390c49f35e0f8165c2326f21c
Salina73 <51583778+Salina73@users.noreply.github.com>
2019-07-18 19:29:33 +0530

    DC

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,61 +1,71 @@
-package com.bridgelab;
+package com.bridgelab.oops;
 import java.util.Random; 
-public class DeckOfCards {
+public class DeckOfCards 
+{
+	static Methods d=new Methods();
+	
     public static void main(String[] args)
-    {       
+    {    
         String[] suits = { "Club", "Diamond", "Heart", "Spades" };
-
         String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace" };
         
-        
-        // initializing of deck   
-        int n = (suits.length) * (ranks.length);
+        DeckOfCard(suits,ranks);
+    }  
+   // initializing of deck   
+    static void DeckOfCard(String[] s, String[] r)
+    {
+    	int n = (s.length) * (r.length);
         String[] deck = new String[n];
         
-        for (int i = 0; i < ranks.length; i++) 
+        for (int i = 0; i < r.length; i++) 
         {
-            for (int j = 0; j < suits.length; j++)
+            for (int j = 0; j < s.length; j++)
             {
-                deck[suits.length*i + j] = ranks[i] + " of " + suits[j];
+                deck[s.length*i + j] = r[i] + " of " + s[j];
             }
         }
-        
-        shuffle(deck,n);
-       }
-    
-        // shuffling of deck
-         private static void shuffle(String[] deck, int n)
-         {
-        	 Random rand = new Random(); 
-        	 for(int i = 0; i < n; i++)
-             {
-             	 int r = i + rand.nextInt(n - i); 
-                 String temp = deck[r];
-                 deck[r] = deck[i];
-                 deck[i] = temp;
-             }
-        	 int start=0,end=8,stop=1;
-        	 distribute(deck,n,start,end,stop);
+        d.DeckOfCard(deck,n);
+    } 	
+}
+class Methods extends DeckOfCards
+{      
+	// shuffling of deck
+    public void DeckOfCard(String[] deck, int n)
+    {
+
+        	Random rand = new Random(); 
+        	 int i=0;
+           	 for(String s:deck)
+                {
+           		 if(i<52) {
+                	 int r = i + rand.nextInt(n - i); 
+                    String temp = deck[r];
+                    deck[r] = deck[i];
+                    deck[i] = temp;
+           		 }
+           		 i++;
+                }
+       	 int start=0,end=8,stop=1;
+         DeckOfCard(deck,n,start,end,stop);
+       
+		
 	}
-
-         //distributing of cards
-
-        private static void distribute(String[] deck, int n, int start, int end,int stop) 
-        {
-        	start=end+1;
-        	end=start+8;   
-        	System.out.println("For player "+stop+" :");
-    		for(int i = start; i <= end; i++)
-            {
-                System.out.println(deck[i]);
-            }
-    		System.out.println();
-    		stop++;
-    		if(stop<5)
-    		{
-    		distribute(deck,n,start,end,stop);
-    		}
-		}
-        
-    }
-
+	
+  //distributing of cards
+     public void DeckOfCard(String[] deck, int n, int start, int end, int stop) 
+     {
+    	 start=end+1;
+     	end=start+8;   
+     	System.out.println("For player "+stop+" :");
+ 		for(int i = start; i <= end; i++)
+         {
+             System.out.println(deck[i]);
+         }
+ 		System.out.println();
+ 		stop++;
+ 		if(stop<5)
+ 		{
+ 			 DeckOfCard(deck,n,start,end,stop);
+ 		}
+}   
+}
